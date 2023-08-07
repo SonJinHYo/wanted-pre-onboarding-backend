f9f7b58559e3c1cbd0ff0bfdccb57f666a1fc303
SonJinHYo <wlsgy95@naver.com>
2023-08-07 15:44:45 +0900

    frature,fix : 비밀번호 암호화 기능 추가 serializers.py에서 비밀번호 기능 추가 추가 과정중 AstrackUser에서 받은 username 필수 필드가 자동 적용되어 models.py 수정 이후 관리자유저 추가할 때 username이 관리자 클래스에 적용되어 있어 username을 제외하지 않고 빈 값으로 두고 진행

--- users/models.py
+++ users/models.py
@@ -4,6 +4,11 @@ from django.core.validators import MinLengthValidator
 
 
 class User(AbstractUser):
+    username = models.CharField(
+        blank=True,
+        null=True,
+        max_length=100,
+    )
     email = models.EmailField(
         unique=True,
     )
@@ -11,3 +16,6 @@ class User(AbstractUser):
         max_length=128,
         validators=[MinLengthValidator],
     )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "password"]
--- users/serializers.py
+++ users/serializers.py
@@ -9,3 +9,13 @@ class UserSerializer(ModelSerializer):
             "email",
             "password",
         )
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        new_user = self.Meta.model(**validated_data)
+
+        if password is not None:
+            new_user.set_password(password)
+        new_user.save()
+
+        return new_user
